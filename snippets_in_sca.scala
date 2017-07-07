ba07ef69ca63839d31119b3f25ae46d9e40c38c7
Adapa venkata sai chaithanya <chaithanya1996@users.noreply.github.com>
2017-07-07 06:31:33 +0000

    updated null value detection snippet

---
+++ snippets_in_sca.scala
@@ -0,0 +1,15 @@
+// --------- Multi Column selection snippet ------------- //
+val col_seq = train_data.columns.slice(2,5).toSeq
+val df = train_data.select(col_seq.head, col_seq.tail: _*)
+df.show(10)
+
+// ------------- Dealing with nulls in col creation ---------//
+val actualDf = sourceDf.withColumn(
+  "is_even",
+  when(
+    col("number").isNotNull, 
+    isEvenSimpleUdf(col("number"))
+  ).otherwise(lit(null))
+)
+
+actualDf.show()
